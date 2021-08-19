d460726e422832b71644177f55015be73708c966
bbendick <bbendick@gmail.com>
2021-08-19 09:02:27 -0700

    Update feedly.sh

--- feedly.sh
+++ feedly.sh
@@ -1,10 +1,7 @@
 # Export opml from feedly
-# Convert from opml to json
+# Convert from opml to json (at https://codebeautify.org/opmlviewer)
 cat feedly.json | jq -rc '.opml.body.outline[]' | while IFS='' read stack; do 
 	ccc=$(echo $stack | jq -r ._text); 
-	#echo $stack | jq ._text;
 	echo "### ${ccc}";
-	#ccc='f-tech';
-	#echo $ccc;
 	jq --arg gg "$ccc" -r '.opml.body.outline[] | select(._text == $gg) | .outline[] | "- [\(._title)](\(._htmlUrl))"' feedly.json;
 done
